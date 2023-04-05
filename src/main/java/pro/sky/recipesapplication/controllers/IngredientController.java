3f34dc8c1fff4520ee5141199478e5c004a0a8b5
Rumonz <lone_assassin@mail.ru>
2023-04-06 01:14:26 +0300

    Добавил нужные библиотеки и поставил аннотации

--- src/main/java/pro/sky/recipesapplication/controllers/IngredientController.java
+++ src/main/java/pro/sky/recipesapplication/controllers/IngredientController.java
@@ -1,5 +1,12 @@
 package pro.sky.recipesapplication.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.recipesapplication.dto.IngredientDTO;
@@ -10,6 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/ingredient")
+@Tag(name = "Ингредиенты", description = "CRUD-операции и другие эндпоинты для работы с ингредиентами")
 public class IngredientController {
     private final IngredientService ingredientService;
 
@@ -18,11 +26,29 @@ public class IngredientController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Поиск ингредиента по id"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Найден ингредиент",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Ingredient.class))
+                            )
+                    }
+            )
+    })
     public IngredientDTO getIngredient(@PathVariable("id") int id) {
         return ingredientService.getIngredient(id);
     }
 
     @GetMapping
+    @Operation(
+            summary = "Вывод всех ингредиентов из базы данных"
+    )
     public ResponseEntity<Map<Integer, Ingredient>> getAllIngredients() {
         Map<Integer, Ingredient> ingredients = ingredientService.getAllIngredients();
         if (ingredients != null) {
@@ -32,11 +58,16 @@ public class IngredientController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Добавление ингредиента в базу данных")
     public IngredientDTO addIngredient(@RequestBody Ingredient ingredient) {
         return ingredientService.addIngredient(ingredient);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Редактирование ингредиента"
+    )
     public ResponseEntity<Ingredient> editIngredient(@PathVariable int id, @RequestBody Ingredient ingredient) {
         Ingredient editedIngredient = ingredientService.editIngredient(id, ingredient);
         if (editedIngredient != null) {
@@ -46,6 +77,8 @@ public class IngredientController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Удаление ингредиента")
     public ResponseEntity<Void> deleteIngredient(@PathVariable int id) {
         if (ingredientService.deleteIngredient(id)) {
             return ResponseEntity.ok().build();
--- src/main/java/pro/sky/recipesapplication/controllers/RecipeController.java
+++ src/main/java/pro/sky/recipesapplication/controllers/RecipeController.java
@@ -1,5 +1,12 @@
 package pro.sky.recipesapplication.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.recipesapplication.dto.IngredientDTO;
@@ -12,6 +19,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/recipe")
+@Tag(name = "Рецепты", description = "CRUD-операции и другие эндпоинты для работы с рецептами")
 public class RecipeController {
     private final RecipeService recipeService;
 
@@ -20,11 +28,29 @@ public class RecipeController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Поиск рецепта по id"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Найден рецепт",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Recipe.class))
+                            )
+                    }
+            )
+    })
     public RecipeDTO getRecipe(@PathVariable("id") int id) {
         return recipeService.getRecipe(id);
     }
 
     @GetMapping
+    @Operation(
+            summary = "Вывод всех рецептов"
+    )
     public ResponseEntity<Map<Integer, Recipe>> getAllRecipes() {
         Map<Integer, Recipe> recipes = recipeService.getAllRecipes();
         if (recipes != null) {
@@ -34,12 +60,18 @@ public class RecipeController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Добавление рецепта"
+    )
     public RecipeDTO addRecipe(@RequestBody Recipe recipe) {
         return recipeService.addRecipe(recipe);
     }
 
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Редактирование рецепта"
+    )
     public ResponseEntity<Recipe> editRecipe(@PathVariable int id, @RequestBody Recipe recipe) {
         Recipe editedRecipe = recipeService.editRecipe(id, recipe);
         if (editedRecipe != null) {
@@ -49,6 +81,9 @@ public class RecipeController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Удаление рецепта"
+    )
     public ResponseEntity<Void> deleteRecipe(@PathVariable int id) {
         if (recipeService.deleteIngredient(id)) {
             return ResponseEntity.ok().build();
