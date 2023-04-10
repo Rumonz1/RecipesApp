5c9081a170e446ea621934462013cf5ae054cd9a
Rumonz <lone_assassin@mail.ru>
2023-04-11 01:07:13 +0300

    поставил аннотации LOMBOK

--- src/main/java/pro/sky/recipesapplication/model/Ingredient.java
+++ src/main/java/pro/sky/recipesapplication/model/Ingredient.java
@@ -1,39 +1,16 @@
 package pro.sky.recipesapplication.model;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
     private String title;
     private int number;
     private String measureUnit;
 
-    public Ingredient(String title, int number, String measureUnit) {
-        this.title = title;
-        this.number = number;
-        this.measureUnit = measureUnit;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getMeasureUnit() {
-        return measureUnit;
-    }
-
-    public void setMeasureUnit(String measureUnit) {
-        this.measureUnit = measureUnit;
-    }
 }
